fed52b41a9572c5712c3fd97494c8f2a8d7e3329
joshelboy <ejtberlin@gmail.com>
2022-05-28 15:09:23 +0200

    Auto refresh

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,17 @@
 import { locate } from "./iss.js";
 import { draw_marker, render } from "./osm.js";
 
-async function initial(){
-    render(map);
-    locate().then((res) => draw_marker(res.latitude, res.longitude, map));
+function initial(){
+    console.log("If you can see this you are truly a developer! Or trying to find vulnerabilities")
+    render();
+    locate().then((res) => draw_marker(res.latitude, res.longitude));
 }
 
-var map = L.map('map').setView([40.736111, 34.473889], 2);
+function second(){
+    locate().then((res) => draw_marker(res.latitude, res.longitude));
+}
+
 initial()
+window.setInterval(function() {
+    second()
+  }, 10000)
--- src/js/osm.js
+++ src/js/osm.js
@@ -1,4 +1,7 @@
-export async function render(map){
+var marker;
+var map = L.map('map').setView([40.736111, 34.473889], 2); //"Center" of the world
+
+export async function render(){
     try {
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
@@ -10,10 +13,13 @@ export async function render(map){
         console.log("Error while initial OSM render")
     }
 }
-
-export function draw_marker(latitude, longitude, map){
+export function draw_marker(latitude, longitude){
     try{
-        var marker = L.marker([latitude, longitude]).addTo(map)
+        if (marker != undefined){
+            map.removeLayer(marker);
+        }
+        marker = L.marker([latitude, longitude]);
+        map.addLayer(marker);
     } catch (err) {
         console.log("Marker couldn't be added to map instance.")
     }
